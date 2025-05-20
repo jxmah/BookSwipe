12f550c9e537fc16901660b4d0bcc698d4c1614d
jxmah <jxmah.23@gmail.com>
2025-05-20 11:41:20 +0200

    Created Google Books API integration with category and rating filters to generateBooks() function

---
+++ src/app/http.js
@@ -0,0 +1,26 @@
+const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
+const API_KEY = process.env.key;
+
+export const generateBooks = async (category, rating) => {
+    let bookFilter = ''; // where the query string is stored
+    if (category) { // if category is provided
+        bookFilter += `subject:${category}`; // add it to the query string
+    }
+
+    if (rating) { // if rating is provided
+        bookFilter += `+averageRating:${rating}`; // add it to the query string
+    }
+
+    const url = new URL(`${BASE_URL}?q=${bookFilter}&key=${API_KEY}`); // create a new URL object
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error("Failed to fetch data");
+    }
+    const data = await response.json();
+    return data;
+};
+
+// google books json names
+// averageRating for rating
+// mainCategory for genre
+// categories for more genres ig
