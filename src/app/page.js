3f21c7c02b3044a72f7d769a7b62d89f027b9c01
jxmah <jxmah.23@gmail.com>
2025-05-20 14:43:45 +0200

    Added main div

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Header></Header>
+      <div id={styles.main} className="container">
+      </div>
     </div>
   );
 }
