e782919127f54e9dd9e02021bce0a87e8a2b2ea7
arissa93 <arissa_rakluea@hotmail.com>
2025-05-27 20:38:20 +0200

    connected foundational interface

--- src/app/my-books/page.js
+++ src/app/my-books/page.js
@@ -1,3 +1,18 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "../components/header";
+import BookForm from "../components/BookForm";
+import BookDetails from "../components/BookDetails";
+
 export default function MyBooks() {
-  return <div>Test my-books route</div>
+  return (
+    <div className={styles.container}>
+      <Header></Header>
+      <div id={styles.main} className="container">
+        
+        
+      </div>
+    </div>
+  );
 }
