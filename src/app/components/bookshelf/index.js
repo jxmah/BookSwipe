62588715523d09513843265130ddea7ecd23c45f
arissa93 <arissa_rakluea@hotmail.com>
2025-05-27 22:46:02 +0200

    start style of bookshelf, added bookshelf component. Started carousel component -unfinished.

---
+++ src/app/components/bookshelf/index.js
@@ -0,0 +1,40 @@
+"use client";
+import styles from "./styles.module.css";
+import { useEffect } from "react";
+
+
+
+const Bookshelf = () => {
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }, []);
+
+    return (
+        <div id={styles.shelfWrapper}>
+            <div class="container text-center">
+                <div className="row">
+                    <div className="col">
+                        Column-book-first
+                    </div>
+                    <div className="col">
+                        Column-book-middle
+                    </div>
+                    <div className="col">
+                        Column-book-last
+                    </div>
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span classNames="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+            i'm in bookshelf component
+        </div>
+    );
+}
+
+export default Bookshelf;
--- src/app/my-books/page.js
+++ src/app/my-books/page.js
@@ -2,16 +2,28 @@ import Image from "next/image";
 import styles from "../page.module.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "../components/header";
-import BookForm from "../components/BookForm";
-import BookDetails from "../components/BookDetails";
+import Bookshelf from "../components/bookshelf";
 
 export default function MyBooks() {
   return (
     <div className={styles.container}>
       <Header></Header>
       <div id={styles.main} className="container">
+        <div id={styles.wrapper}>
+          wrapper-mybooks
+          <Bookshelf></Bookshelf>
+
+          <div id={styles.shelf}>wrapper</div>
+
+          
+        </div>
+
         
         
+
+
+
+
       </div>
     </div>
   );
