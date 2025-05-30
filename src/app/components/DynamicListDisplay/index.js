79daef32f87e076af4109ddf1df9441a8ad3f5fc
arissa93 <arissa_rakluea@hotmail.com>
2025-05-30 15:57:20 +0200

    fetch all information in localstorage:lists

--- src/app/components/DynamicListDisplay/index.js
+++ src/app/components/DynamicListDisplay/index.js
@@ -5,20 +5,33 @@ import styles from "./styles.module.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect } from "react";
 
-export default function DynamicListDisplay({ params }) {
-    /*
+export default function DynamicListDisplay() {
     const [lists, setLists] = useState([]);
 
     useEffect(() => {
-        const referList = localStorage.getItem("lists");
-        setLists(JSON.parse(referList))
-
+        const storedLists = localStorage.getItem("lists");
+        if (storedLists) {
+            setLists(JSON.parse(storedLists));
+        }
     }, []);
-    console.log(referList)*/
 
     return (
+
+
+
+
         <div className={styles.container}>
-            <p>Lorem ipsum</p>
+            <p>Lorem ipsum {lists.name}</p>
+
+            <ul className={styles.savedList}>
+                {lists.map((list, index) => (
+                        <li key={index}>
+                            <p className={styles.listName}>{list.name}</p>
+                            <p className={styles.itemsAmount}>Först -- {list.items.length} Book--- Last</p>
+                        </li>
+                ))}
+            </ul>
+
             <div className={styles.shelf}></div>
         </div>
     )
