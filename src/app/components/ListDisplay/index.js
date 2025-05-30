c0037a7bcbe9c91a6e5acd5325d5f9b16169f361
jxmah <jxmah.23@gmail.com>
2025-05-31 00:41:48 +0200

    Fixed listDisplay styling and added overflow (scroll bar)

--- src/app/components/ListDisplay/index.js
+++ src/app/components/ListDisplay/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import styles from "./styles.module.css";
 import { useRouter } from 'next/navigation'
 
-const ListDisplay = ({ lists = [], onDropBook}) => {
+const ListDisplay = ({ lists = [], onDropBook }) => {
     const router = useRouter();
 
     const handleDrop = (e, listName) => {
@@ -16,20 +16,21 @@ const ListDisplay = ({ lists = [], onDropBook}) => {
             onDropBook(listName, book);
         }
     };
-    
+
     console.log("felsökning på onDropBook: ", onDropBook);
     console.trace("onDropBook trace");
+
     return (
         <div className={`${styles.createdListsWrapper} overflow-auto`}>
             <ul className={styles.savedList}>
                 {lists.map((list, index) => (
-                    <button 
-                    className={styles.listItem} 
-                    key={index} 
-                    type="button" 
-                    onClick={() => router.push(`/pages/my-books/${list.name}`)}
-                    onDragOver={(e) => e.preventDefault()}
-                    onDrop={(e) => handleDrop(e, list.name)}
+                    <button
+                        className={styles.listItem}
+                        key={index}
+                        type="button"
+                        onClick={() => router.push(`/pages/my-books/${list.name}`)}
+                        onDragOver={(e) => e.preventDefault()}
+                        onDrop={(e) => handleDrop(e, list.name)}
                     >
                         <li key={index}>
                             <p className={styles.listName}>{list.name}</p>
