61ed36bea0b6635b8675bb21f9637f3a8feb11e6
arissa93 <arissa_rakluea@hotmail.com>
2025-05-30 12:18:36 +0200

    connect dynamic route with my-books

--- src/app/pages/my-books/[listName]/page.js
+++ src/app/pages/my-books/[listName]/page.js
@@ -7,15 +7,12 @@ import { useRouter } from 'next/navigation'
 import Header from "../../../components/Header";
  
 export default function ListName() {
-  const router = useRouter()
- 
-  return (
+   return (
     <div className={styles.container}>
       <Header></Header>
       <div id={styles.main} className="container">
         <h1>dynamic route</h1>
       </div>
     </div>
-    
   )
 }
